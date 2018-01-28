912f1987b7c0580dde03d2ff25d159a8924bbe3a
Maharsh Patel <patel@maharsh.net>
2018-01-28 08:04:40 -0700

    moved stuff

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, render_template, request, redirect
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<senderID>")
+def redirect_code(senderID):
+    return render_template("receive.html", senderID=senderID)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
