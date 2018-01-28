6f52f40edb2255c8dbb00518da8ea7164ebbd0a0
Maharsh Patel <patel@maharsh.net>
2018-01-27 21:12:32 -0700

    start flask server

---
+++ web/test/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<senderID>")
+def redirect_code(senderID):
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
